510d2596cd61393275810b35f85a42cf8bec92b0
Kacpero60 <kapiw2002@gmail.com>
2025-05-02 12:19:28 +0200

    Update HomeScreen.tsx

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome, Kacper 👋</Text>
+      <Text style={styles.title}>Welcome, Kacper </Text>
       <Text style={styles.subtitle}>Choose a category to start learning</Text>
     </View>
   );
